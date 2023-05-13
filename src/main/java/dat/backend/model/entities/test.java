4b01818e232d942bcd91f66f00c826af56a55cb6
JuliusLansner <axiiss@live.dk>
2023-05-14 01:54:17 +0200

    Imported 3D framework

---
+++ src/main/java/dat/backend/model/entities/test.java
@@ -0,0 +1,12 @@
+package dat.backend.model.entities;
+import org.abstractica.javacsg.Geometry3D;
+import org.abstractica.javacsg.JavaCSG;
+import org.abstractica.javacsg.JavaCSGFactory;
+public class test {
+    public static void main(String[] args)
+    {
+        JavaCSG csg = JavaCSGFactory.createDefault();
+        Geometry3D cyl = csg.cylinder3D(10, 20, 32, true);
+        csg.view(cyl);
+    }
+}
