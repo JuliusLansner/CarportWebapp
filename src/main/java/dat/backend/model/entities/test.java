485965b8f102faf21f06f5068206e64f0bc2429f
JuliusLansner <axiiss@live.dk>
2023-05-14 01:56:32 +0200

    Update test.java

--- src/main/java/dat/backend/model/entities/test.java
+++ src/main/java/dat/backend/model/entities/test.java
@@ -3,6 +3,9 @@ import org.abstractica.javacsg.Geometry3D;
 import org.abstractica.javacsg.JavaCSG;
 import org.abstractica.javacsg.JavaCSGFactory;
 public class test {
+    // Added lines to XML
+    // manually added external libraries from javaCSG
+    //should work as intended.
     public static void main(String[] args)
     {
         JavaCSG csg = JavaCSGFactory.createDefault();
