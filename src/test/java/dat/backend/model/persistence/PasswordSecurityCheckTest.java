b8f8ecf5ef13fbe43abbc073659bda1e94dcf8ff
JuliusLansner <axiiss@live.dk>
2023-05-16 11:28:12 +0200

    commit

--- src/test/java/dat/backend/model/persistence/PasswordSecurityCheckTest.java
+++ src/test/java/dat/backend/model/persistence/PasswordSecurityCheckTest.java
@@ -1,5 +1,6 @@
 package dat.backend.model.persistence;
 
+import dat.backend.model.services.PasswordSecurityCheck;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
